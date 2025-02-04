fbfbcdcdd370962223ca465ba1af4b8a894710c4
irie.masataka <irie.masataka@sed.co.jp>
2025-02-04 11:13:37 +0900

    (update) 引数と返り値の変更
    引数で明示的に気体識別し、読み込むh5ファイルパスも与える。
    返り値はX_NUMの数×5のサイズのリスト。

--- src/Read_h5_Bias.py
+++ src/Read_h5_Bias.py
@@ -12,28 +12,18 @@ from logging import getLogger
 logger = getLogger("Log").getChild("sub")
 
 ##@brief SWFPプロダクトのHDF5形式ファイルを読み込み、補正パラメータを取得
-##@param [in]  id 気体識別　id=CO2,CH4,CO
-##@param [in]  Bias_Num バイアス補正パラメータ個数
+##@param [in]  X データセットパス
+##@param [in]  Num バイアス補正パラメータ個数
+##@param [in]  h5path h5ファイルパス
 ##@return バイアス補正パラメータデータセット
-def Read_Bias_Param(id, sysin):
-
-    h5path = sysin.PRODFILEPATH_SWFP
+def Read_Bias_Param(X, Num, h5path):
 
     with h5py.File(h5path,'r') as f:
-        if id == 'CO2': #CO2データ入力
-            X = sysin.CO2_X         # 補正パラメータのデータセットパスを取得
-            Num = sysin.CO2_X_NUM   # 補正パラメータのデータセットパスの数
-        elif id == 'CH4':
-            X = sysin.CH4_X
-            Num = sysin.CH4_X_NUM
-        elif id == 'CO':
-            X = sysin.CO_X
-            Num = sysin.CO_X_NUM
 
+        Bias_Param_Dataset = []
         for ii in range(Num):
             X_str = X[ii] # データセットのパス NUM の個数だけ（上限29）ある
             X_str_list = X_str.split(',') # データセットのカンマ区切りをリストに分割
-            Bias_Param_Dataset = []
             for data_path in list(X_str_list):
                 if data_path == "none":
                     Bias_Param_Dataset.append([""])
@@ -43,5 +33,5 @@ def Read_Bias_Param(id, sysin):
                     except KeyError:
                         logger.error('バイアス補正パラメータのデータセットパスに誤りがあります。{}'.format(data_path))
                         return id
-            return Bias_Param_Dataset
+        return Bias_Param_Dataset
 
